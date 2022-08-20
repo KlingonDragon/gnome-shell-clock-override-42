0040f354e44e431c4507845d87728249d53f5995
KlingonDragon <klingondragon@outlook.com>
2022-08-21 00:17:24 +0100

    Working Gnome 42 files

--- test/install-jasmine-gjs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -ex
-
-tempdir=`mktemp --directory --tmpdir install-jasmin.XXXXXXXXXX`
-git clone https://github.com/ptomato/jasmine-gjs $tempdir
-cd $tempdir
-./autogen.sh
-make
-sudo make install
--- test/run-tests.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-jasmine --config verbose.json
